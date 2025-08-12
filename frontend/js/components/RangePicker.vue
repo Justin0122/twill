d86a2c0d26e58c4b9dbd2702812e0f0b82e8e93c
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-12 14:47:29 +0200

    rangepicker

--- frontend/js/components/RangePicker.vue
+++ frontend/js/components/RangePicker.vue
@@ -11,7 +11,7 @@
         v-model="internalValue"
       />
       <span class="range-picker__max">{{ max }}</span>
-      <span class="range-picker__value">{{ modelValue }}</span>
+      <span class="range-picker__value">{{ internalValue }}</span>
     </div>
   </div>
 </template>
