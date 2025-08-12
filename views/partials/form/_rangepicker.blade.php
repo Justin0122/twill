83f4d969ec1788a3b1904a1fb61464f8428dfa8d
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-12 15:11:55 +0200

    rangepicker

--- views/partials/form/_rangepicker.blade.php
+++ views/partials/form/_rangepicker.blade.php
@@ -1,18 +1,18 @@
 <a17-rangepicker
     label="{{ $label }}"
-    {!! $formFieldName() !!}
     :min="{{ $min ?? 0 }}"
     :max="{{ $max ?? 100 }}"
     :step="{{ $step ?? 1 }}"
-    :value="{{ old($name, $default ?? 0) }}"
-    :disabled="{{ $disabled ? 'true' : 'false' }}"
-    @if ($required) :required="true" @endif
-    @if ($note) note="{{ $note }}" @endif
+    default-value="{{ $default ?? 0 }}"
+    {!! $formFieldName() !!}
     in-store="value"
 ></a17-rangepicker>
 
 @unless($renderForBlocks || $renderForModal || (!isset($item->$name) && is_null($formFieldsValue = getFormFieldsValue($form_fields, $name, $default))))
-@push('vuexStore')
-    @include('twill::partials.form.utils._selector_input_store')
-@endpush
+    @push('vuexStore')
+        window['{{ config('twill.js_namespace') }}'].STORE.form.fields.push({
+        name: '{{ $name }}',
+        value: {!! json_encode($item->$name ?? $formFieldsValue ?? $default) !!}
+        })
+    @endpush
 @endunless
