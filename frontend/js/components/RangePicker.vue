71b99c7a57daff2daef82f187075814a9f19f6fb
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-12 14:38:58 +0200

    rangepicker

--- frontend/js/components/RangePicker.vue
+++ frontend/js/components/RangePicker.vue
@@ -7,18 +7,10 @@
         :min="min"
         :max="max"
         :step="step"
-        :value="modelValue"
+        :value="value"
         @input="onInput"
       />
-      <span class="range-picker__value">{{ modelValue }}</span>
-    </div>
-    <div class="range-picker__ticks">
-      <span
-        v-for="tick in ticks"
-        :key="tick"
-        class="range-picker__tick"
-        :style="{ left: tickPosition(tick) + '%' }"
-      >|</span>
+      <span class="range-picker__value">{{ value }}</span>
     </div>
   </div>
 </template>
@@ -27,27 +19,30 @@
   export default {
     name: 'A17RangePicker',
     props: {
-      title: { type: String, default: '' },
-      min: { type: Number, default: 0 },
-      max: { type: Number, default: 100 },
-      modelValue: { type: Number, default: 0 },
-      step: { type: Number, default: 1 }
-    },
-    computed: {
-      ticks() {
-        const ticks = []
-        for (let i = this.min; i <= this.max; i += this.step) {
-          ticks.push(i)
-        }
-        return ticks
+      title: {
+        type: String,
+        default: ''
+      },
+      min: {
+        type: Number,
+        default: 0
+      },
+      max: {
+        type: Number,
+        default: 100
+      },
+      value: {
+        type: Number,
+        default: 0
+      },
+      step: {
+        type: Number,
+        default: 1
       }
     },
     methods: {
       onInput(event) {
-        this.$emit('update:modelValue', Number(event.target.value))
-      },
-      tickPosition(tick) {
-        return ((tick - this.min) / (this.max - this.min)) * 100
+        this.$emit('input', Number(event.target.value))
       }
     }
   }
@@ -73,17 +68,5 @@
     min-width: 40px;
     text-align: center;
     font-weight: bold;
-  }
-  .range-picker__ticks {
-    position: relative;
-    height: 12px;
-    margin-top: 4px;
-  }
-  .range-picker__tick {
-    position: absolute;
-    top: 0;
-    transform: translateX(-50%);
-    color: #888;
-    font-size: 12px;
   }
 </style>
--- views/partials/form/_rangepicker.blade.php
+++ views/partials/form/_rangepicker.blade.php
@@ -1,21 +1,18 @@
-<div>
-    <label>{{ $label }}</label>
-    <a17-rangepicker
-        {!! $formFieldName() !!}
-        :min="{{ $minValue }}"
-        :max="{{ $maxValue }}"
-        :step="{{ $stepValue }}"
-        v-model="formFields['{{ $name }}']"
-        :disabled="{{ $disabled ? 'true' : 'false' }}"
-        @if ($required) :required="true" @endif
-        @if ($note) note="{{ $note }}" @endif
-        in-store="value"
-    ></a17-rangepicker>
-    <div>Current: @{{ formFields['{{ $name }}'] }}</div>
-</div>
+<a17-rangepicker
+    label="{{ $label }}"
+    {!! $formFieldName() !!}
+    :min="{{ $min ?? 0 }}"
+    :max="{{ $max ?? 100 }}"
+    :step="{{ $step ?? 1 }}"
+    :value="{{ old($name, $default ?? 0) }}"
+    :disabled="{{ $disabled ? 'true' : 'false' }}"
+    @if ($required) :required="true" @endif
+    @if ($note) note="{{ $note }}" @endif
+    in-store="value"
+></a17-rangepicker>
 
 @unless($renderForBlocks || $renderForModal || (!isset($item->$name) && is_null($formFieldsValue = getFormFieldsValue($form_fields, $name, $default))))
-    @push('vuexStore')
-        @include('twill::partials.form.utils._selector_input_store')
-    @endpush
+@push('vuexStore')
+    @include('twill::partials.form.utils._selector_input_store')
+@endpush
 @endunless
