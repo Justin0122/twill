6184946b5583f8331bf5298f1166d92b8efd8ada
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-12 14:04:20 +0200

    added max and min to rangepicker

--- views/partials/form/_rangepicker.blade.php
+++ views/partials/form/_rangepicker.blade.php
@@ -1,15 +1,19 @@
-<a17-rangepicker
-    label="{{ $label }}"
-    {!! $formFieldName() !!}
-    :min="{{ $min ?? 0 }}"
-    :max="{{ $max ?? 100 }}"
-    :step="{{ $step ?? 1 }}"
-    :value="{{ old($name, $default ?? 0) }}"
-    :disabled="{{ $disabled ? 'true' : 'false' }}"
-    @if ($required) :required="true" @endif
-    @if ($note) note="{{ $note }}" @endif
-    in-store="value"
-></a17-rangepicker>
+<div>
+    <label>{{ $label }}</label>
+    <a17-rangepicker
+        {!! $formFieldName() !!}
+        :min="{{ $minValue }}"
+        :max="{{ $maxValue }}"
+        :step="{{ $stepValue }}"
+        :value="{{ old($name, $default ?? $minValue) }}"
+        :disabled="{{ $disabled ? 'true' : 'false' }}"
+        @if ($required) :required="true" @endif
+        @if ($note) note="{{ $note }}" @endif
+        in-store="value"
+        @input="currentValue = $event"
+    ></a17-rangepicker>
+    <div>Current: @{{ currentValue }}</div>
+</div>
 
 @unless($renderForBlocks || $renderForModal || (!isset($item->$name) && is_null($formFieldsValue = getFormFieldsValue($form_fields, $name, $default))))
 @push('vuexStore')
