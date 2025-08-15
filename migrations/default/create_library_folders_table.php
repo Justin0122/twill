ab96adb70e1b7a28d17164e97e27711b6a4460a4
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-15 10:52:13 +0200

    library folders table

--- migrations/default/create_library_folders_table.php
+++ migrations/default/create_library_folders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
@@ -15,7 +16,6 @@ return new class extends Migration {
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
             $table->index(['library', 'parent_id']);
-            $table->unique(['path'], 'library_folders_path_unique', 191);
         });
     }
 
