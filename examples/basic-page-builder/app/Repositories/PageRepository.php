a8b1bfb0b3df1a2c86d5652889c563ec4bea54a8
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-25 14:11:34 +0200

    save grid layout

--- examples/basic-page-builder/app/Repositories/PageRepository.php
+++ examples/basic-page-builder/app/Repositories/PageRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use A17\Twill\Models\Block;
 use A17\Twill\Repositories\Behaviors\HandleBlocks;
 use A17\Twill\Repositories\Behaviors\HandleTranslations;
 use A17\Twill\Repositories\Behaviors\HandleSlugs;
@@ -17,5 +18,35 @@ class PageRepository extends ModuleRepository
     public function __construct(Page $model)
     {
         $this->model = $model;
+    }
+
+    public function afterSave($object, $request): void
+    {
+        $layouts = $request->input('blocks_layout', []); // array: editorName => json/string
+
+        foreach ($layouts as $editorName => $layout) {
+            if (is_string($layout)) {
+                $layout = json_decode($layout, true) ?: [];
+            }
+            if (! is_array($layout)) {
+                continue;
+            }
+
+            $byId = collect($layout)->keyBy('id');
+
+            Block::where('blockable_type', $object->getMorphClass())
+                ->where('blockable_id', $object->id)
+                ->where('editor_name', $editorName)
+                ->get()
+                ->each(function (Block $block) use ($byId) {
+                    if (! $byId->has((array) $block->id)) {
+                        return;
+                    }
+                    $content = $block->content ?? [];
+                    $content['grid'] = $byId[$block->id]['grid'] ?? null; // ['x','y','w','h']
+                    $block->content = $content;
+                    $block->save();
+                });
+        }
     }
 }
