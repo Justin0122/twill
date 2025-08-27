0f2c1cb576aef522c97da59615dee9ec637fb23c
Justin Jongstra <j.jongstra@tuwi.nl>
2025-08-27 11:29:04 +0200

    added slight gap inbetween columns

--- views/partials/form/utils/_columns.blade.php
+++ views/partials/form/utils/_columns.blade.php
@@ -1,8 +1,8 @@
 @php
-    $colStyle = (isset($middle) || isset($middleFields)) ? '' : 'width: 50%; box-sizing: border-box; display: inline-block; vertical-align: top;';
+    $colStyle = (isset($middle) || isset($middleFields)) ? '' : 'width: 49%; box-sizing: border-box; display: inline-block; vertical-align: top;';
     $colClassAttr = (isset($middle) || isset($middleFields)) ? 'col--third col--third-wrap' : '';
 @endphp
-<div class="wrapper" style="margin-left: 0; white-space: nowrap;">
+<div class="wrapper" style="margin-left: 0; white-space: nowrap; gap:0.6rem;">
     <div class="{{ $colClassAttr }}" @if($colStyle)style="{{ $colStyle }}"@endif>
         @isset($leftFields)
             @foreach($leftFields as $field)
