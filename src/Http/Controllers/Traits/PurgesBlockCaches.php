d8cda8e76e1669aa18cf8e8cf358c16c5e704afa
Justin Jongstra <j.jongstra@tuwi.nl>
2025-09-12 14:37:46 +0200

    cache

--- src/Http/Controllers/Traits/PurgesBlockCaches.php
+++ src/Http/Controllers/Traits/PurgesBlockCaches.php
@@ -3,51 +3,24 @@
 namespace A17\Twill\Http\Controllers\Traits;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Cache;
 
 trait PurgesBlockCaches
 {
     protected function purgeBlockCachesFor(string $modelType, int|string $pageId, ?string $editorName = null): void
     {
-        $table  = config('cache.stores.database.table', 'cache');
-        $prefix = config('cache.prefix', 'laravel_cache');
+        $table = config('cache.stores.database.table', 'cache');
+        $regexp = "Page_{$pageId}_[^0-9]";
 
-        $esc = fn (string $v) => str_replace(['%', '_'], ['\\%', '\\_'], $v);
-
-        $modelType  = $esc($modelType);
-        $pageId     = $esc((string)$pageId);
-        $editorName = $editorName ? $esc($editorName) : null;
-
-        $patterns = [
-            "{$prefix}block_renderer_{$modelType}_{$pageId}_%",
-        ];
-
-        if ($editorName) {
-            $patterns[] = "{$prefix}block_renderer_{$modelType}_{$pageId}_{$editorName}";
-        }
-
-        foreach ($patterns as $pattern) {
-            $keys = DB::table($table)
-                ->where('key', 'like', $pattern)
-                ->pluck('key');
-
-            foreach ($keys as $key) {
-                Cache::forget($key);
-            }
-        }
+        DB::delete("DELETE FROM `{$table}` WHERE `key` REGEXP ?", [$regexp]);
     }
+
 
     protected function purgeAllBlockPreviews(): void
     {
-        $table  = config('cache.stores.database.table', 'cache');
+        $table = config('cache.stores.database.table', 'cache');
         $prefix = config('cache.prefix', 'laravel_cache');
 
-        $keys = DB::table($table)
-            ->where('key', 'like', "{$prefix}_block_preview\\_%")
-            ->pluck('key');
-
-        foreach ($keys as $key) {
-            Cache::forget($key);
-        }
+        DB::delete("DELETE FROM `{$table}` WHERE `key` LIKE ?", ["{$prefix}_block_preview\\_%"]);
     }
+
 }
